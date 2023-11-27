8001c772eb29c177e0a8c4a02b4cc1ae6d29cf53
Rafsan M <89879376+rafsan24@users.noreply.github.com>
2023-11-27 18:32:01 -0500

    Update floatx.c

--- floatx.c
+++ floatx.c
@@ -3,33 +3,11 @@
 #include <limits.h> // for CHAR_BIT - number of bits per byte
 #include <math.h> // for isinf and isnan
 #include <stdio.h>
-#include "bitFields.h" // from HW04
+#include "bitFields.h" 
 
 
 floatx doubleToFloatx(double val,int totBits,int expBits) 
 {
-
-/*-----------------------------------------------------------------------------------------------
-	From the README: 	At a high level, doing the conversion requires several manipulations:
-	1. Extracting the sign bit from the double value, and inserting it into the floatx value at
-		the correct position.
-	2. Handle special cases, such as 0.0, infinity, or not-a-number.
-	3. Extract the biased exponent from the double value.
-			Check to see if the double value is sub-normal. If so, handle it.
-			Check to make sure the floatx exponent won't overflow or underflow).
-					If so, handle as a special case.
-			If not, rebias the exponent using the floatx bias (which depends on the number of exponent bits)
-			and write the result to the correct location in the floatx result.
-	4. Extract the fraction bits from the double value.
-			Determine how many bits are available for the fraction in the floatx value,
-			and truncate or extend the original value,
-			and write the resulting bits to the floatx result.
-	6. Return the floatx result.
-----------------------------------------------------------------------------------------------------*/
-
-	// First, make some assertions to ensure the totBits and expBits parameters are OK
-	// Then, implement the algorithm
-   	
    	assert(1 <= expBits && expBits <= (totBits - 2));
 	assert(3 <= totBits && totBits <= 64);
 
